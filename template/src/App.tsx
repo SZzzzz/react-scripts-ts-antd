0363c84b92ce4d12f30b541abeb0ab161aaabe25
SZ <353678810@qq.com>
2017-10-31 22:05:36 +0800

    fix: replace css with scss.

--- template/src/App.tsx
+++ template/src/App.tsx
@@ -1,5 +1,5 @@
-import * as React from 'react';
-import './App.css';
+import React from 'react';
+import './App.scss';
 
 const logo = require('./logo.svg');
 
--- template/src/index.tsx
+++ template/src/index.tsx
@@ -1,8 +1,8 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import './index.css';
+import './index.scss';
 
 ReactDOM.render(
   <App />,
