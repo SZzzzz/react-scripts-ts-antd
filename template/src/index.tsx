56e35075185bb0e48dcdaac37633c9f020c71602
SZ <353678810@qq.com>
2018-02-01 11:19:42 +0800

    feat: update template.

--- template/src/index.tsx
+++ template/src/index.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import './index.scss';
+import './index.css';
 
 ReactDOM.render(
   <App />,
