68e4891844b234b5697d5d755805c8361252bf8b
SZ <353678810@qq.com>
2018-04-16 22:06:48 +0800

    fix: fix loaders.

--- config/loaders.js
+++ config/loaders.js
@@ -230,7 +230,7 @@ const fileLoader = {
   // it's runtime that would otherwise processed through "file" loader.
   // Also exclude `html` and `json` extensions so they get processed
   // by webpacks internal loaders.
-  exclude: [/\.js$/, /\.html$/, /\.json$/],
+  exclude: [/\.(js|jsx|mjs)$/, /\.html$/, /\.json$/],
   options: {
     name: 'static/media/[name].[hash:8].[ext]',
   },
@@ -238,6 +238,7 @@ const fileLoader = {
 
 module.exports = {
   urlLoader,
+  jsLoader,
   tsLoader,
   cssLoaderDev,
   cssLoaderProd,
